635c4f9b6ee4317e545cfc7bed0b48670ead9096
Bryan Cattle <bryan.cattle@gmail.com>
2015-04-06 13:26:09 -0700

    Error handling: script now bails if something fails to download.

--- build-frameworks.sh
+++ build-frameworks.sh
@@ -8,8 +8,12 @@
 
 echo 'Fetching podspec files'
 
-wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/CocoaLumberjack/2.0.0-beta4/CocoaLumberjack.podspec.json
-echo '.\c'
+#wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/JRSwizzle/1.0/JRSwizzle.podspec.json
+#echo '.\c'
+#wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/NSLogger/1.5.1/NSLogger.podspec.json
+#echo '.\c'
+#wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/CocoaLumberjack/2.0.0-beta4/CocoaLumberjack.podspec.json
+#echo '.\c'
 wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/Mantle/1.5.1/Mantle.podspec.json
 echo '.\c'
 wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/AFNetworking/2.5.0/AFNetworking.podspec.json
@@ -24,31 +28,47 @@ wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/Nimbus/1
 echo '.\c'
 wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/OCMock/3.1.1/OCMock.podspec.json 
 echo '.\c'
+wget -qN https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/FBSnapshotTestCase/1.5/FBSnapshotTestCase.podspec.json
+echo '.\c'
 echo ''
 
-echo 'Building CocoaLumberjack'
-pod package CocoaLumberjack.podspec.json --force
+echo 'Setting up Cocoapods'
+sudo gem install cocoapods -v
+sudo gem install cocoapods-packager -v
+pod setup
+
+#echo 'Building JRSwizzle'
+#pod package JRSwizzle.podspec.json --force || exit 1
+
+#echo 'Building NSLogger'
+#pod package NSLogger.podspec.json --force || exit 1
+
+#echo 'Building CocoaLumberjack'
+#pod package CocoaLumberjack.podspec.json --force || exit 1
 
 echo 'Building Mantle'
-pod package Mantle.podspec.json --force
+pod package Mantle.podspec.json --force || exit 1
 
 echo 'Building AFNetworking'
-pod package AFNetworking.podspec.json --force
+pod package AFNetworking.podspec.json --force || exit 1
 
 echo 'Building SDWebImage'
-pod package SDWebImage.podspec.json --force
+pod package SDWebImage.podspec.json --force || exit 1
 
 echo 'Building PromiseKit'
-pod package PromiseKit.podspec.json --force
+pod package PromiseKit.podspec.json --force || exit 1
 
 echo 'Building TTTAttributedLabel'
-pod package TTTAttributedLabel.podspec.json --force
+pod package TTTAttributedLabel.podspec.json --force || exit 1
 
 echo 'Building Numbus/PagingScrollView'
-pod package Nimbus.podspec.json --subspecs=PagingScrollView --force
+pod package Nimbus.podspec.json --subspecs=PagingScrollView --force || exit 1
 
 echo 'Building OCMock'
-pod package OCMock.podspec.json --force
+pod package OCMock.podspec.json --force || exit 1
+
+echo 'Building FBSnapshotTestCase'
+pod package FBSnapshotTestCase.podspec.json --force || exit 1
 
 echo 'Downloading AWS SDK'
 wget -N http://sdk-for-ios.amazonwebservices.com/latest/aws-ios-sdk.zip
